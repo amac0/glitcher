c64b3ceb54a24dc932ee76fc1c46b575819fad46
Glitch (intelligent-insidious-concavenator) <none>
2021-04-01 19:05:12 +0000

    💂🏛 back to testing with some Apple Music code

--- do_checks.sh
+++ do_checks.sh
@@ -22,11 +22,11 @@ export PYTHONUNBUFFERED=true
 
 # Run a glorious Python 3 server
 #$VIRTUALENV/bin/gunicorn attnfeeddj:app --access-logfile '-' --log-level 'debug'
-#while [ true ]
-#  do
-#    /usr/bin/wget --quiet --output-document .data/last_check.txt https://intelligent-insidious-concavenator.glitch.me/check
+while [ true ]
+  do
+    /usr/bin/wget --quiet --output-document .data/last_check.txt https://intelligent-insidious-concavenator.glitch.me/check
+    /usr/bin/wget --quiet --output-document .data/last_searches.txt https://intelligent-insidious-concavenator.glitch.me/process_searches
+    sleep 60
+  done
 #    /usr/bin/wget --quiet --output-document .data/last_replies.txt https://intelligent-insidious-concavenator.glitch.me/process_replies
-#    /usr/bin/wget --quiet --output-document .data/last_searches.txt https://intelligent-insidious-concavenator.glitch.me/process_searches
-#    sleep 60
-#  done
-#/usr/bin/wget --quiet --output-document .data/last_timeslines.txt https://intelligent-insidious-concavenator.glitch.me/process_timelines
+#    /usr/bin/wget --quiet --output-document .data/last_timeslines.txt https://intelligent-insidious-concavenator.glitch.me/process_timelines
